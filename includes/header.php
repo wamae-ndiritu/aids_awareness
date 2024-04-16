49cd9dd334e5fa85c4fdd833e11961eaddfda13d
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-16 17:08:36 +0300

    Add loggin buttons on homepage

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,10 @@
+<?php
+
+session_start();
+// Get user information from session
+$user_id = $_SESSION["user_id"];
+?>
+
 <nav class="w-full flex items-center justify-between bg-red-500 px-4 py-2 md:px-16">
     <a href="index.php" class="flex gap-3 items-center">
         <img src="images/hiv-awareness-ribbon.jpeg" alt="logo" class="h-16 object-cover">
@@ -14,6 +21,17 @@
             <a href="index.php#join-our-community">Joining Our Community</a>
         </li>
         <li><a href="index.php#call-to-action">Tips</a></li>
+        <?php
+        if ($user_id){
+             echo "<li><a href='logout.php'
+                class='bg-white text-red-600 px-4 py-2 rounded hover:bg-gray-900 hover:text-white'>Logout</a></li>";    
+        }else{
+            echo "<li><a href='register.php'
+                class='bg-white text-red-600 px-4 py-2 rounded hover:bg-gray-900 hover:text-white'>Sign Up</a></li>
+        <li><a href='login.php' class='bg-white text-red-600 px-4 py-2 rounded hover:bg-gray-900 hover:text-white'>Sign
+                In</a></li>";
+        }
+        ?>
         <li></li>
     </ul>
 </nav>
